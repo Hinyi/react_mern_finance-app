5989035bc7fd129980a3b906e2482674baf191da
Hinyi <mrmck16@gmail.com>
2024-12-18 23:24:45 +0100

    Added first chart

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -1,15 +1,77 @@
 import DashboardBox from "@/components/DashboardBox";
 import { useGetKpisQuery } from "@/state/api";
-import React from "react";
+import { useTheme } from "@emotion/react";
+import { useMemo } from "react";
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  ReferenceLine,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 type Props = {};
 
 const Row1 = (props: Props) => {
-  const {data} = useGetKpisQuery();
-  
+  const { palette } = useTheme();
+  const { data } = useGetKpisQuery();
+  console.log("🚀 ~ Row1 ~ data:", data);
+  const revenueExpenses = useMemo(() => {
+    return (
+      data &&
+      data[0].monthlyData.map(({ month, revenue, expenses }) => {
+        return {
+          name: month.substring(0, 3),
+          revenue: revenue,
+          expenses: expenses,
+        };
+      })
+    );
+  }, [data]);
+
   return (
     <>
-      <DashboardBox gridArea="a">aa</DashboardBox>
+      <DashboardBox gridArea="a">
+        <ResponsiveContainer width="100%" height="100%">
+          <AreaChart
+            data={revenueExpenses}
+            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+          >
+            <XAxis
+              dataKey="name"
+              tickLine={false}
+              style={{ fontSize: "10px" }}
+            />
+            <YAxis
+              tickLine={false}
+              style={{ fontSize: "10px" }}
+              axisLine={{strokeWidth: "0"}}
+              domain={[8000,23000]}
+            />
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="revenue"
+              dot={true}
+              stroke={palette.primary.main}
+              fillOpacity={1}
+              fill="url(#colorRevenue)"
+            />
+            <Area
+              type="monotone"
+              dataKey="expenses"
+              dot={true}
+              stroke={palette.primary.main}
+              fillOpacity={1}
+              fill="url(#colorExpenses)"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </DashboardBox>
       <DashboardBox gridArea="b">aa</DashboardBox>
       <DashboardBox gridArea="c">aa</DashboardBox>
     </>
